8dce1e8439043a7ae06c488c8461431fddc70179
Sridhar Kannan <sridharkannankool@gmail.com>
2024-10-22 11:02:55 +0000

    Completed stack - Largest Rectangle In Histogram

--- stack/hard/LargestRectangleinHistogram.java
+++ stack/hard/LargestRectangleinHistogram.java
@@ -6,80 +6,44 @@ public class LargestRectangleinHistogram {
 
     public static int largestRectangleArea(int[] heights) {
 
-        int minHeight = 0;
-        int horizontalArea = 0;
-        int rectArea = 0;
-
-        Stack<int[]> stack = new Stack<>(); // height, index, maxArea
+        Stack<int[]> stack = new Stack<>(); // index , height
+        int maxArea = 0;
 
         for (int i = 0; i < heights.length; i++) {
+            
+            int start = i;
 
-            int currentHeight = heights[i];
-
-            if (stack.isEmpty()) {
-                minHeight = currentHeight;
-                horizontalArea = minHeight * (i + 1);
-                rectArea = horizontalArea;
-
-                stack.push(
-                        new int[] {
-                                currentHeight, i, Math.max(currentHeight, Math.max(horizontalArea, rectArea)),
-                                currentHeight
-                        });
-                continue;
+            while( !stack.isEmpty() && stack.peek()[1] > heights[i]){
+                int[] top = stack.pop();
+                int index = top[0];
+                int height = top[1];
+                maxArea = Math.max(maxArea, height * (i - index));
+                start = index;
             }
-
-            int topHeight = stack.peek()[0];
-            int topIndex = stack.peek()[1];
-            int topArea = stack.peek()[2];
-            int minStackheight = stack.peek()[3];
-
-            minHeight = Math.min(minHeight, Math.min(currentHeight, topHeight));
-            horizontalArea = minHeight * (i + 1);
-
-            rectArea = Math.min(currentHeight,minStackheight) * (i - topIndex + 1);
-
-            if (currentHeight <= topHeight) {
-                stack.push(
-                        new int[] {
-                                currentHeight, i,
-                                Math.max(topArea, Math.max(currentHeight, Math.max(horizontalArea, rectArea))),
-                                Math.min(currentHeight, minStackheight),
-                        });
-            } else {
-
-                int height = Math.min(currentHeight, minStackheight);
-
-                if (stack.size() > 1) {
-                    stack.clear();
-
-                    height = currentHeight;
-
-                }
-
-                stack.push(
-                        new int[] {
-                                currentHeight, i,
-                                Math.max(topArea, Math.max(currentHeight, Math.max(horizontalArea, rectArea))), height
-                        });
-            }
-
+            stack.push(new int[] {start, heights[i]});
         }
 
-        return stack.peek()[2];
+        for(int[] pair : stack){
+            int index = pair[0];
+            int height = pair[1];
+            maxArea = Math.max(maxArea, height * (heights.length - index));
+        }
+
+
+        return maxArea;
     }
 
-    
+
 
 
     @SuppressWarnings("unused")
     public static void main(String[] args) {
 
-        int[] testCase = { 2, 1, 5, 6, 2, 3 };
+        int[] testCase1 = { 2, 1, 5, 6, 2, 3 };
         int[] testCase2 = { 2, 4 };
         int[] testCase3 = { 7,1,7,2,2,4 };
 
-        System.out.println(largestRectangleArea(testCase3));
+        System.out.println(largestRectangleArea(testCase1));
 
     }
 }
